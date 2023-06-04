d1fa7a756a55beea98cbfe5e1fc4562d82c2728d
Juan Camilo Granados <juanca_grave@hotmail.com>
2023-06-03 19:04:29 -0600

    prueba4 ajustes

--- bixoParser.py
+++ bixoParser.py
@@ -1525,7 +1525,7 @@ parser = yacc.yacc()
 # Procesar cada línea con el parser
 
 
-fileName = "prueba3.txt"   
+fileName = "prueba4.txt"   
 inputFile = open(fileName, 'r')
 inputCode = inputFile.read()
 inputFile.close()
